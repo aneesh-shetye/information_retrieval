64c3387188b90c5c0729c0154f2a9bd056641b1d
aneesh-shetye <axs10302@nyu.edu>
2024-06-25 04:03:52 -0400

    changes to utils get_text function and minor changes to make_text_docs

--- utils/utils.py
+++ utils/utils.py
@@ -8,6 +8,10 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 def preprocess_text(text: str):
   '''
   basic preprocessing, just keep the large text blocks, i.e,
@@ -20,16 +24,34 @@ def preprocess_text(text: str):
   result_text = '\n\n'.join(long_text_blocks)
   return result_text
 
+def fetch_content2(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Run headless Chrome
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+    content = driver.page_source
+    driver.quit()
+    return content
+
+def fetch_content(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # This will raise an HTTPError for bad responses
+    return response
+
 def get_text(url: str):
 
   try:
-    response = requests.get(url)
-    response.raise_for_status() #raise exception for HTTP errors
+    response = fetch_content(url)
+    # response.raise_for_status() #raise exception for HTTP errors
     if response.status_code != 200: 
       print("Failed to retrieve text")
       return None 
 
-    soup = bs(response.content, 'html.parser')
+    soup = bs(response.text, 'html.parser')
     #text = soup.get_text()
     if soup is None:
       print("Failed to parse text")
@@ -43,4 +65,12 @@ def get_text(url: str):
 
   except requests.exceptions.RequestException as e:
     print(f'An error occured: {e}')
+    response = fetch_content2(url)
+    # soup = bs(response.text, 'html.parser')
+    
+    if response is not None: 
+      # paragraphs = soup.find_all('p')
+      # text = "\n\n".join([para.get_text() for para in paragraphs])
+      return preprocess_text(response)
+
     return None
